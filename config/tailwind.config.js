c078500cbef104069e7da5550426c07e292c80f8
gassuncao27 <gassuncao27@gmail.com>
2024-04-01 14:34:31 -0300

    Application model and store

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -22,3 +22,18 @@ module.exports = {
     require('@tailwindcss/container-queries'),
   ]
 }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
